79c69cedcb0413c34ce58ccd7dec451d654984a9
Maximo Barone <maximo.barone.arg@gmail.com>
2026-03-21 12:41:15 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculadoraCripto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
