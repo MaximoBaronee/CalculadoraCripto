22e002816c815f161c5c2c2b5167a6026217f2c1
Maximo Barone <maximo.barone.arg@gmail.com>
2026-04-13 23:19:45 -0300

    adaptacion a postgresql para hacer el deploy a railway

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,11 +5,10 @@ namespace CalculadoraCripto.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<Operacion> Operaciones { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite("Data Source=cripto.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
